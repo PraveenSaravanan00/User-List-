17bd0b79df6ec422c2c19103565134c565667a2b
praveen <praveen.s@mitrahsoft.com>
2024-07-26 19:02:12 +0530

    done

---
+++ UserList/Controllers/UserList.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserList.Models;
+using Microsoft.Extensions.Configuration;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel;
+using static UserList.Models.showUserList;
+
+namespace UserList.Controllers
+{
+    public class UserList : Controller
+    {
+        private IConfiguration _configuration;
+
+        public UserList(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult UserLists()
+        {
+            return View();
+        }
+        public IActionResult ShowUserLists()
+        {
+            showUserList userlists = new showUserList(_configuration);
+            ViewBag.table = userlists.showdata();
+            return View();
+
+
+        }
+        public IActionResult Editdata(UserListData user,int userid) {
+            int id = userid;
+            string firstname = user.firstName;
+            string lastname = user.lastName;
+            int salary = user.salary;
+            string gender = user.gender;
+            showUserList userlists = new showUserList(_configuration);
+            Console.WriteLine(userid);
+            userlists.EditData(userid, firstname, lastname, salary, gender);
+            ViewBag.table = userlists.showdata();
+            return RedirectToAction("ShowUserLists", "UserList");
+        }
+        [HttpPost]
+        public IActionResult addUser(UserListData user )
+        {
+            string firstname = user.firstName;
+            string lastname= user.lastName;
+            int salary=user.salary;
+            string gender=user.gender;
+            showUserList userlists = new showUserList(_configuration);
+            userlists.AddData( firstname, lastname, salary, gender);
+            ViewBag.table = userlists.showdata();
+            return RedirectToAction("ShowUserLists", "UserList");
+        }
+        public IActionResult DeleteData(int id)
+        {
+            showUserList userlists = new showUserList(_configuration);
+            userlists.DeleteData( id);
+            ViewBag.table = userlists.showdata();
+            return RedirectToAction("ShowUserLists", "UserList");
+        }
+        public IActionResult CreateUser(int userid=0, string firstName="", string lastName = "", int salary = 0, string gender = "")
+        {
+            ViewBag.action = "addUser";
+            if (userid != 0)
+            {
+                ViewBag.action = "Editdata";
+            }
+            ViewBag.userid = userid;
+            
+            return View();
+        }
+    }
+}
+
+
---
+++ UserList/Models/UserListModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace UserList.Models
+{
+    public class showUserList
+    {
+        private readonly IConfiguration _configuration;
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+
+        public showUserList(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public object showdata()
+        {
+            string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("select * from userlistdata", connection);
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            connection.Close();
+            return ds;
+        }
+        public void AddData( string firstname,string lastname,int salary,string gender)
+        {
+            string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("insert into userlistdata(firstname,lastname,salary,gender)" + "values (@firstname,@lastname,@salary,@gender)", connection);
+            connection.Open();
+            cmd.Parameters.AddWithValue("@firstname", firstname);
+            cmd.Parameters.AddWithValue("@lastname", lastname);
+            cmd.Parameters.AddWithValue("@salary", salary);
+            cmd.Parameters.AddWithValue("@gender", gender);
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+        public void DeleteData(int id)
+        {
+            string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("delete from userlistdata where userid=@id;", connection);
+            connection.Open();
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+        public void EditData(int userid, string firstName, string lastName, int salary, string gender)
+        {
+            string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("UPDATE userlistdata SET firstname = @firstname, lastname= @lastname, salary=@salary ,gender=@gender where  userid=@id", connection);
+            connection.Open();
+            cmd.Parameters.AddWithValue("@id", userid);
+            cmd.Parameters.AddWithValue("@firstname", firstName);
+            cmd.Parameters.AddWithValue("@lastname", lastName);
+            cmd.Parameters.AddWithValue("@salary", salary);
+            cmd.Parameters.AddWithValue("@gender", gender);
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+        public class UserListData
+        {
+            public int userid { get; set; }
+            public string firstName{get; set;}
+            public string lastName { get; set;}
+            public int salary { get; set;}
+
+            public string gender { get; set;}
+         
+        }
+
+    }
+
+    }
